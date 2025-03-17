6d278b4a5e95cd3891d5b13d56b6dfddf34ae445
ashik <ashikaju01@gmail.com>
2025-03-17 10:42:20 +0530

    shell scripting

---
+++ aws_resourse_tracker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#ashk ali
+#27-2-25
+#aws resourse usage
+#aws s3
+#aws ec2
+#aws lambda
+
+#list s3 buckets
+aws s3 ls
+
+#list ec2 instances
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+#list lambda
+aws lambda list-functions
+
+#list iam user
+aws iam list-users
+
