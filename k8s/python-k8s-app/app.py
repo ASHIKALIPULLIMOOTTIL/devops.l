1b49f8af9238e40211b881a664b4c2fd63758953
UBUNTU <ashikaju01@gmail.com>
2026-05-11 00:23:35 +0530

    added aws

--- k8s/python-k8s-app/app.py
+++ k8s/python-k8s-app/app.py
@@ -2,9 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def read_file(path):
+    try:
+        with open(path, "r") as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        return "not found"
+
 @app.route("/")
 def hello():
-    return "Hello! Python app running on Kubernetes 🚀 --ASHIK ALI"
+    app_name = read_file("/config/APP_NAME")
+    app_env = read_file("/config/APP_ENV")
+    db_user = read_file("/secrets/DB_USER")
+
+    return f"""
+    App Name: {app_name}<br>
+    Environment: {app_env}<br>
+    DB User: {db_user}
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
